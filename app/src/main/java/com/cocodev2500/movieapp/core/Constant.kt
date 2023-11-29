358211d09b6ebfd5fe3fd51e34b7abf2d8f29fa3
ERICK ESCOBAR <101473187+GuitHub75@users.noreply.github.com>
2023-11-29 17:14:55 -0600

    Update Constant.kt

--- app/src/main/java/com/cocodev2500/movieapp/core/Constant.kt
+++ app/src/main/java/com/cocodev2500/movieapp/core/Constant.kt
@@ -1,7 +1,7 @@
 package com.cocodev2500.movieapp.core
 
 object Constant {
-    const val API_KEY = "02cb9bdeb1e5e7073b1459c0540bb0e1"
+    const val API_KEY = "YURE_APK_FROM_TMDB"
     const val BASE_URL = "https://api.themoviedb.org/3/movie/"
     const val BASE_IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
 
